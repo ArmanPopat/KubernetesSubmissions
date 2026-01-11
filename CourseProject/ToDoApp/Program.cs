a98d1217cf1b4c043b4a55a651a0120ad19b96da
Arman Popat <armanpopat@hotmail.com>
2026-01-11 16:00:52 +0000

    initial commit for Course Project

---
+++ CourseProject/ToDoApp/Program.cs
@@ -0,0 +1,42 @@
+
+namespace ToDoApp;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var builder = WebApplication.CreateBuilder(args);
+        var port = Environment.GetEnvironmentVariable("PORT");
+        if (!string.IsNullOrWhiteSpace(port) && int.TryParse(port, out var portNumber))
+        {
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.ListenAnyIP(portNumber);
+            });
+        }
+        else {
+            throw new InvalidOperationException("Invalid or unset PORT environment variable");
+        }
+
+        // Add services to the container.
+        builder.Services.AddAuthorization();
+
+        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        builder.Services.AddOpenApi();
+
+        var app = builder.Build();
+
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.MapOpenApi();
+        }
+        
+        app.UseAuthorization();
+        
+        Console.WriteLine("Server started in port {0}",port);
+
+        app.Run();
+
+    }
+}
