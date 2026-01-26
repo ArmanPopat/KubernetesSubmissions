5b0a4788297dce05c9bd456f59d8fd3dc39d4e9d
Arman Popat <armanpopat@hotmail.com>
2026-01-26 01:07:34 +0000

    1.7

--- LogOutput/LogOutput/Program.cs
+++ LogOutput/LogOutput/Program.cs
@@ -2,13 +2,29 @@
 
 class Program
 {
-    static async Task Main(string[] args)
+    static void Main(string[] args)
     {
         var randString = Guid.NewGuid().ToString();
-        using var timer = new PeriodicTimer(TimeSpan.FromSeconds(5));
-        while (await timer.WaitForNextTickAsync())
+        
+        var builder = WebApplication.CreateBuilder(args);
+        var port = Environment.GetEnvironmentVariable("PORT");
+        if (!string.IsNullOrWhiteSpace(port) && int.TryParse(port, out var portNumber))
         {
-            Console.WriteLine($"{DateTime.UtcNow:O}: {randString}");
+            builder.WebHost.ConfigureKestrel(options =>
+            {
+                options.ListenAnyIP(portNumber);
+            });
         }
+        else {
+            throw new InvalidOperationException("Invalid or unset PORT environment variable");
+        }
+        var app = builder.Build();
+        app.MapGet("/", () => $"{DateTime.UtcNow:O}: {randString}");
+        // using var timer = new PeriodicTimer(TimeSpan.FromSeconds(5));
+        // while (await timer.WaitForNextTickAsync())
+        // {
+        //     Console.WriteLine($"{DateTime.UtcNow:O}: {randString}");
+        // }
+        app.Run();
     }
 }
